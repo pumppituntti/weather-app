9708d37c8eb65fa0252b3855e29ccbbc0d335101
Artem <artem.tolpa@gmail.com>
2022-05-13 13:57:21 +0300

    Added search by city and displaying data on the screen.

--- app/src/main/java/fi/tuni/weatherapp/MainActivity.kt
+++ app/src/main/java/fi/tuni/weatherapp/MainActivity.kt
@@ -1,9 +1,16 @@
 package fi.tuni.weatherapp
 
+import android.app.Activity
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.databind.ObjectMapper
 import java.io.BufferedReader
@@ -13,21 +20,56 @@ import java.net.URL
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var input: EditText
+    lateinit var result_header: TextView
+    lateinit var result_info: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        input = findViewById(R.id.input)
+        result_header = findViewById(R.id.result_header)
+        result_info = findViewById(R.id.result_info)
+
         val button = findViewById<Button>(R.id.button_find)
         button.setOnClickListener {
-            downloadUrlAsync(this, "https://api.openweathermap.org/data/2.5/weather?q=tampere&units=metric&appid=223d2e7247b5a5b808c39b7c173269ae"){
-                Log.d("hello", it)
-                val mp = ObjectMapper()
-                val myObject: WeatherObject = mp.readValue(it, WeatherObject::class.java)
-                Log.d("hello", myObject.name.toString())
-                Log.d("hello", myObject.wind?.speed.toString())
-                Log.d("hello", myObject.weather?.get(0)?.main.toString())
+            hideKeyboard()
+            if (input.text.isEmpty()) {
+                Log.d("hello", "here")
+                Toast.makeText(this, "Field should not be empty!", Toast.LENGTH_LONG).show()
+            } else {
+                result_header.text = "Loading..."
+                val city = input.text.toString()
+                downloadUrlAsync(
+                    this,
+                    "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=223d2e7247b5a5b808c39b7c173269ae"
+                ) {
+                    val mp = ObjectMapper()
+                    val myObject: WeatherObject = mp.readValue(it, WeatherObject::class.java)
+                    Log.d("hello", myObject.name.toString())
+                    Log.d("hello", myObject.wind?.speed.toString())
+                    Log.d("hello", myObject.weather?.get(0)?.main.toString())
+                    Log.d("hello", input.text.toString())
+                    result_header.text = "Weather in ${myObject.name.toString()}"
+                    result_info.text =
+                        "${myObject.weather?.get(0)?.main.toString()}, ${myObject.main?.temp} °C\n" +
+                                "(feels like ${myObject.main?.feels_like.toString()} °C)\n" +
+                                "Wind: ${myObject.wind?.speed.toString()}"
+                }
             }
         }
+    }
+
+    fun Activity.hideKeyboard() {
+        hideKeyboard(currentFocus ?: View(this))
+    }
+
+    fun Context.hideKeyboard(view: View) {
+        val inputMethodManager =
+            getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     private fun downloadUrlAsync(
@@ -47,6 +89,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class WeatherWind(var speed: String? = null) {}
 
@@ -54,7 +97,16 @@ data class WeatherWind(var speed: String? = null) {}
 data class WeatherMain(var temp: String? = null, var feels_like: String? = null) {}
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class WeatherInfo(var main: String? = null, var description: String? = null, var icon: String? = null) {}
+data class WeatherInfo(
+    var main: String? = null,
+    var description: String? = null,
+    var icon: String? = null
+) {}
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class WeatherObject(var name: String? = null, var weather: MutableList<WeatherInfo>? = null, var main: WeatherMain? = null, var wind: WeatherWind? = null) {}
+data class WeatherObject(
+    var name: String? = null,
+    var weather: MutableList<WeatherInfo>? = null,
+    var main: WeatherMain? = null,
+    var wind: WeatherWind? = null
+) {}
