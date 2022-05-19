35eb787b24afd980591a5cd472b409388b2e8a7c
Artem <artem.tolpa@gmail.com>
2022-05-19 18:39:37 +0300

    Little UI refactor

--- app/src/main/java/fi/tuni/weatherapp/MainActivity.kt
+++ app/src/main/java/fi/tuni/weatherapp/MainActivity.kt
@@ -5,7 +5,6 @@ import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
